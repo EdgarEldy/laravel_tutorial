138b19ca4e68ec53483158758210ffcb4bd91133
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-03-17 20:51:13 +0200

    Added categories route

--- routes/web.php
+++ routes/web.php
@@ -10,37 +10,34 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//Register a route
 
-Route::get('/', function () {
-    return view('home/index');
-});
+//Route::get('/',function (){
+//   return view('home');
+//});
 
-Route::get('orders', function()
-{
+//Home
+Route::get('/','HomeController@index');
 
-});
+//Orders
+Route::get('orders','OrdersController@index');
+Route::get('orders/create','OrdersController@create');
 
-Route::get('customers', function()
-{
+//Customers
 
-});
+//Products
 
-Route::get('products', function()
-{
+//Categories
+Route::get('categories','CategoriesController@index');
+Route::get('categories/create','CategoriesController@create');
+Route::post('categories','CategoriesController@store');
+Route::get('categories/edit/{category}','CategoriesController@edit');
+Route::put('categories/{category}','CategoriesController@update');
+Route::post('categories/destroy/{category}','CategoriesController@destroy');
+//Profiles
 
-});
+//Users
 
-Route::get('categories', function()
-{
+//Auth::routes();
 
-});
 
-Route::get('profiles', function()
-{
-
-});
-
-Route::get('users', function()
-{
-
-});
