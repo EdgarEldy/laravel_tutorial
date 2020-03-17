23e848c46491e6475a7c383193ffe47eb5de2a7e
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-03-17 21:12:49 +0200

    Added crud methods

---
+++ app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use Illuminate\Http\Request;
+
+class CategoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $categories = Category::all();
+        return view('categories.index', [
+                'categories' => $categories
+            ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('categories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $category = new Category();
+        $category->cat_name = $request->cat_name;
+        $category->save();
+        return redirect('categories');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $category = Category::find($id);
+        return view('categories.edit', ['category' => $category]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update($id)
+    {
+        //
+        $category = Category::find($id);
+        $category->cat_name = \request('cat_name');
+        $category->save();
+        return redirect('categories');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $category = Category::find($id);
+        $category->delete();
+        return redirect('categories');
+    }
+}
