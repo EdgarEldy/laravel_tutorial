884d1032c145bc478bc6fba26b6c117b1d445607
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-03-17 20:50:19 +0200

    Added a form to update a category

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -1,31 +1,20 @@
 @extends('templates.default')
 @section('content')
 
-    <div class="row">
-        <ol class="breadcrumb">
-            <li><a href="#"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
-            <li class="active">Centre d'appel | ECONET-LEO</li>
-        </ol>
-    </div><!--/.row-->
-    <!--
-    <div class="row">
-        <div class="col-lg-12">
-            <h1 class="page-header">Gestion des courriers</h1>
-        </div>
-    </div> <row-->
-
     <div class="col-lg-12">
         <div class="panel panel-default">
-            <div class="panel-heading">Modifier</div>
+            <div class="panel-heading">Edit a product category</div>
             <div class="panel-body">
                 <div class="col-md-6">
-                    <form role="form" action="<?= URL_ROOT; ?>/categories/edit/<?= $data['id']; ?>" method="post" enctype="multipart/form-data">
+                    <form role="form" action="/categories/{{$category->id}}" method="POST">
+                        @csrf
+                        @method('PUT')
 
                         <div class="form-group">
-                            <label>Product category</label>
-                            <input type="text" name="product_name" class="form-control <?= (!empty($data['product_name_err'])) ? 'is_invalid' : '';  ?>"
-                                   placeholder="" value="<?= $data['product_name']; ?>">
-                            <span class="invalid-feedback"><?= $data['product_name_err'] ?></span>
+                            <label>Category name :</label>
+                            <input type="text" name="cat_name" class="form-control"
+                                   placeholder="" value="{{$category->cat_name}}">
+                            <span class="invalid-feedback"></span>
                         </div>
                         <button type="submit" id="submit" class="btn btn-primary">Save</button>
                         <button type="reset" class="btn btn-default">Reset</button>
@@ -34,12 +23,6 @@
             </div>
         </div>
     </div><!-- /.col-->
-    <script>
-        function closed()
-        {
-            window.opener.location.reload();
-            window.close();
-        }
-    </script>
+
 
 @endsection()
