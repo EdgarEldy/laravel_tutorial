9d1b03ccd9032e5c512b5aa723510d1512f4bef5
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-04-21 20:51:51 +0200

    Added new fields

--- database/migrations/2020_03_13_194901_create_customers_table.php
+++ database/migrations/2020_03_13_194901_create_customers_table.php
@@ -15,7 +15,9 @@ class CreateCustomersTable extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
             $table->string('tel');
             $table->string('address');
             $table->timestamps();
