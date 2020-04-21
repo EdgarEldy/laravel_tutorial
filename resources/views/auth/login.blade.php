d89a5489002404092bd71231b840a810c1fa41c1
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-04-21 20:36:51 +0200

    Updated login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,13 +1,12 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
+<!doctype html>
+<html lang="en">
+@include('inc.head')
+<body>
+<div class="row">
+    <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
+        <div class="login-panel panel panel-default">
+            <div class="panel-heading">Laravel tutorial | STORE MANAGEMENT SYSTEM  </div>
+            <div class="panel-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
@@ -18,7 +17,7 @@
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
+                                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
@@ -32,7 +31,7 @@
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
+                                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
@@ -69,5 +68,6 @@
             </div>
         </div>
     </div>
-</div>
-@endsection
+</body>
+</html>
+
