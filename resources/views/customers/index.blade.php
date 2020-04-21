6417338a124bf6a4194f986986a8d755e0234cf4
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-04-21 20:52:04 +0200

    Updated customers homepage

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -4,7 +4,7 @@
         <div class="panel panel-default">
             <div class="panel-heading">Customers</div>
             <div class="panel-body">
-                <a href="<?= URL_ROOT;?>/customers/add"
+                <a href="{{url('customers/add')}}"
                    class="btn btn-primary">New</a>
                 <table data-toggle="table" data-url=""  data-show-refresh="true" data-show-toggle="true"
                        data-show-columns="true" data-search="true" data-select-item-name="toolbar1" data-pagination="true"
@@ -19,24 +19,24 @@
                     </tr>
                     </thead>
                     <tbody>
-                    <?php foreach ($data['customers'] as $customer) :?>
+                    @foreach($customers as $customer)
                     <tr>
-                        <td><?= $customer->customer_id; ?> </td>
-                        <td><?= $customer->name; ?></td>
-                        <td><?= $customer->tel; ?></td>
-                        <td><?= $customer->address; ?></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
                         <td>
                             <div class="card-footer">
-                                <a href="<?= URL_ROOT;?>/customers/edit/<?= $customer->customer_id?>"
-                                   class="btn btn-primary">Modifier</a>
-                                <form action="<?= URL_ROOT;?>/customers/delete/<?= $customer->customer_id; ?>" method="post">
+                                <a href=""
+                                   class="btn btn-primary">Edit</a>
+                                <form action="" method="post">
                                     <button type="submit" onclick="return confirm('Are you sure you want to delete this customer ?')" class="btn btn-danger btn-sm">Delete</button>
                                 </form>
 
                             </div>
                         </td>
                     </tr>
-                    <?php endforeach;?>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
