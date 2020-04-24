714b577469a44f3d497257018fd4b6189ef5cc67
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-04-24 23:04:33 +0200

    Added a form to update a customer

--- resources/views/customers/edit.blade.php
+++ resources/views/customers/edit.blade.php
@@ -1,30 +1,39 @@
 @extends('templates.default')
 @section('content')
+
     <div class="col-lg-12">
         <div class="panel panel-default">
-            <div class="panel-heading">Update a customer</div>
+            <div class="panel-heading">Edit a customer</div>
             <div class="panel-body">
                 <div class="col-md-6">
-                    <form role="form" action="<?= URL_ROOT; ?>/customers/edit/<?= $data['id']; ?>" method="post" enctype="multipart/form-data">
+                    <form role="form" action="/customers/{{$customer->id}}" method="POST">
+                        @csrf
+                        @method('PUT')
 
                         <div class="form-group">
-                            <label>Customer name :</label>
-                            <input type="text" name="name" class="form-control <?= (!empty($data['name_err'])) ? 'is-invalid' : ''; ?>"
-                                   placeholder="" value="<?= $data['name']; ?>">
-                            <span class="invalid-feedback"><?= $data['name_err'] ?></span>
+                            <label>First name :</label>
+                            <input type="text" name="first_name" class="form-control" class=""
+                                   placeholder="" value="{{$customer->first_name}}">
+                        </div>
+                        <div class="form-group">
+                            <label>First name :</label>
+                            <input type="text" name="last_name" class="form-control" class=""
+                                   placeholder="" value="{{$customer->last_name}}">
+                        </div>
+                        <div class="form-group">
+                            <label>Email :</label>
+                            <input type="text" name="email" class="form-control" class=""
+                                   placeholder="" value="{{$customer->email}}">
                         </div>
                         <div class="form-group">
                             <label>Tel :</label>
-                            <input type="text" name="tel" class="form-control <?= (!empty($data['tel_err'])) ? 'is-invalid' : ''; ?>"
-                                   placeholder="Telephone" value="<?= $data['tel']; ?>">
-                            <span class="invalid-feedback"><?= $data['tel_err'] ?></span>
+                            <input type="text" name="tel" class="form-control" class=""
+                                   placeholder="" value="{{$customer->tel}}">
                         </div>
-
                         <div class="form-group">
                             <label>Address :</label>
-                            <input type="text" name="address" class="form-control <?= (!empty($data['address_err'])) ? 'is-invalid' : ''; ?>"
-                                   placeholder="" value="<?= $data['address']; ?>">
-                            <span class="invalid-feedback"><?= $data['address_err'] ?></span>
+                            <input type="text" name="address" class="form-control" class=""
+                                   placeholder="" value="{{$customer->address}}">
                         </div>
                         <button type="submit" id="submit" class="btn btn-primary">Save</button>
                         <button type="reset" class="btn btn-default">Reset</button>
@@ -33,5 +42,6 @@
             </div>
         </div>
     </div><!-- /.col-->
+
 
 @endsection()
