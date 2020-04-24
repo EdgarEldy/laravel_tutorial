8f3a76f36ba29e9cf047ffe003d84496de53dc48
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-04-24 23:15:50 +0200

    Added form errors

--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -9,28 +9,43 @@
                         @csrf
                         <div class="form-group">
                             <label>First name :</label>
-                            <input type="text" name="first_name" class="form-control"
+                            <input type="text" name="first_name" class="form-control" class="@error('first_name') is-invalid @enderror"
                                    placeholder="" value="">
+                            @error('first_name')
+                            <div class="btn btn-danger">{{$message}}</div>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label>Last name :</label>
-                            <input type="text" name="last_name" class="form-control"
+                            <input type="text" name="last_name" class="form-control" class="@error('last_name') is-invalid @enderror"
                                    placeholder="" value="">
+                            @error('last_name')
+                            <button class="btn-danger">{{$message}}</button>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label>Email :</label>
-                            <input type="text" name="email" class="form-control"
+                            <input type="text" name="email" class="form-control" class="@error('email') is-invalid @enderror"
                                    placeholder="" value="">
+                            @error('email')
+                            <button class="btn-danger">{{$message}}</button>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label>Tel :</label>
-                            <input type="text" name="tel" class="form-control"
+                            <input type="text" name="tel" class="form-control" class="@error('tel') is-invalid @enderror"
                                    placeholder="" value="">
+                            @error('tel')
+                            <button class="btn-danger">{{$message}}</button>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label>Address :</label>
-                            <input type="text" name="address" class="form-control"
+                            <input type="text" name="address" class="form-control" class="@error('address') is-invalid @enderror"
                                    placeholder="" value="">
+                            @error('address')
+                            <button class="btn-danger">{{$message}}</button>
+                            @enderror
                         </div>
                         <button type="submit" id="submit" class="btn btn-primary">Save</button>
                         <button type="reset" class="btn btn-default">Reset</button>
--- resources/views/customers/edit.blade.php
+++ resources/views/customers/edit.blade.php
@@ -12,28 +12,43 @@
 
                         <div class="form-group">
                             <label>First name :</label>
-                            <input type="text" name="first_name" class="form-control" class=""
+                            <input type="text" name="first_name" class="form-control" class="@error('first_name') is-invalid @enderror"
                                    placeholder="" value="{{$customer->first_name}}">
+                            @error('first_name')
+                            <button class="btn-danger">{{$message}}</button>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label>First name :</label>
-                            <input type="text" name="last_name" class="form-control" class=""
+                            <input type="text" name="last_name" class="form-control" class="@error('last_name') is-invalid @enderror"
                                    placeholder="" value="{{$customer->last_name}}">
+                            @error('last_name')
+                            <button class="btn-danger">{{$message}}</button>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label>Email :</label>
-                            <input type="text" name="email" class="form-control" class=""
+                            <input type="text" name="email" class="form-control" class="@error('email') is-invalid @enderror"
                                    placeholder="" value="{{$customer->email}}">
+                            @error('email')
+                            <button class="btn-danger">{{$message}}</button>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label>Tel :</label>
-                            <input type="text" name="tel" class="form-control" class=""
+                            <input type="text" name="tel" class="form-control" class="@error('tel') is-invalid @enderror"
                                    placeholder="" value="{{$customer->tel}}">
+                            @error('tel')
+                            <button class="btn-danger">{{$message}}</button>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <label>Address :</label>
-                            <input type="text" name="address" class="form-control" class=""
+                            <input type="text" name="address" class="form-control" class="@error('address') is-invalid @enderror"
                                    placeholder="" value="{{$customer->address}}">
+                            @error('address')
+                            <button class="btn-danger">{{$message}}</button>
+                            @enderror
                         </div>
                         <button type="submit" id="submit" class="btn btn-primary">Save</button>
                         <button type="reset" class="btn btn-default">Reset</button>
