000b60523eadf10b980e762d25648dde679b696a
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-06-12 23:57:31 +0200

    Added oneToMany relationship related to Order model

--- app/Product.php
+++ app/Product.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     //
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function orders()
+    {
+        return $this->hasMany('App\Order');
+    }
 }
