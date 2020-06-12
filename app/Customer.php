96d8a2d2fd80403b884b9be6e48480b1c2bcc712
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-06-12 23:57:18 +0200

    Added oneToMany relationship related to Order model

--- app/Customer.php
+++ app/Customer.php
@@ -8,4 +8,12 @@ class Customer extends Model
 {
     //
     protected $guarded = [];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function orders()
+    {
+        return $this->hasMany('App\Order');
+    }
 }
