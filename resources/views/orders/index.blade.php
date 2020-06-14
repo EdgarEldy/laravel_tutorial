b54b6d135004f5abe81ac3d2bf5a79110788224a
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-06-14 23:53:52 +0200

    Update orders datatable

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -2,9 +2,9 @@
 @section('content')
     <div class="col-lg-12">
         <div class="panel panel-default">
-            <div class="panel-heading">orders</div>
+            <div class="panel-heading">Orders</div>
             <div class="panel-body">
-                <a href="{{url('orders/add')}}"
+                <a href="{{url('orders/create')}}"
                    class="btn btn-primary">New</a>
                 <table data-toggle="table" data-url=""  data-show-refresh="true" data-show-toggle="true"
                        data-show-columns="true" data-search="true" data-select-item-name="toolbar1" data-pagination="true"
@@ -12,35 +12,38 @@
                     <thead>
                     <tr>
                         <th>ID</th>
-                        <th>Customer name</th>
-                        <th>product name</th>
+                        <th>First name</th>
+                        <th>Last name</th>
+                        <th>Product name</th>
                         <th>Unit price</th>
-                        <th>qty</th>
+                        <th>Qty</th>
                         <th>Total</th>
                         <th>Options</th>
                     </tr>
                     </thead>
                     <tbody>
-                    <?php foreach ($data['orders'] as $order) :?>
-                    <tr>
-                        <td><?= $order->order_id; ?> </td>
-                        <td><?= $order->name; ?></td>
-                        <td><?= $order->product_name; ?></td>
-                        <td><?= $order->unit_price; ?></td>
-                        <td><?= $order->qty; ?></td>
-                        <td><?= $order->total; ?></td>
-                        <td>
-                            <div class="card-footer">
-                                <a href="{{url('orders/edit/')}}"
-                                   class="btn btn-primary">Edit</a>
-                                <form action="<?= URL_ROOT;?>/orders/delete/<?= $order->order_id; ?>" method="post">
-                                    <button type="submit" onclick="return confirm('Are you sure you want to delete this order ?')" class="btn btn-danger btn-sm">Delete</button>
-                                </form>
+                    @foreach($orders as $order)
+                        <tr>
+                            <td>{{$order->id}}</td>
+                            <td>{{$order->first_name}}</td>
+                            <td>{{$order->last_name}}</td>
+                            <td>{{$order->product_name}}</td>
+                            <td>{{$order->unit_price}}</td>
+                            <td>{{$order->qty}}</td>
+                            <td>{{$order->total}}</td>
+                            <td>
+                                <div class="card-footer">
+                                    <a href="orders/edit/{{$order->id}}"
+                                       class="btn btn-primary">Edit</a>
+                                    <form action="orders/destroy/{{$order->id}}" method="post">
+                                        @csrf
+                                        <button type="submit" onclick="return confirm('Are you sure you want to delete this order ?')" class="btn btn-danger btn-sm">Delete</button>
+                                    </form>
 
-                            </div>
-                        </td>
-                    </tr>
-                    <?php endforeach;?>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
                     </tbody>
                 </table>
             </div>
