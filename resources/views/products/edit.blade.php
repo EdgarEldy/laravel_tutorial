755cd81d4bd5a6d4187bd78a46b6c7b9d8533818
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-07-23 23:18:36 +0200

    Getting the category id and the category name by product id

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -6,35 +6,40 @@
             <div class="panel-heading">Edit a product</div>
             <div class="panel-body">
                 <div class="col-md-6">
-                    <form role="form" action="/products/{{$product->id}}" method="POST">
+                    <form role="form" action="/products/{{ $product->id }}" method="POST">
                         @csrf
                         @method('PUT')
                         <div class="form-group">
-                            <label for="">Select a category :</label>
-                            <select name="category_id" id="" class="form-control" class="@error('category_id') is-invalid @enderror">
+                            <label for="">Category name :</label>
+                            <select name="category_id" id="" class="form-control"
+                                class="@error('category_id') is-invalid @enderror">
+                                <option value="{{ $category->id }}">{{ $category->cat_name }}</option>
                                 <option value=""></option>
+                                <option value="">Select a category...</option>
                                 @foreach($categories as $category)
-                                    <option value="{{$category->id}}">{{$category->cat_name}}</option>
+                                    <option value="{{ $category->id }}">{{ $category->cat_name }}</option>
                                 @endforeach
                             </select>
                             @error('category_id')
-                            <button class="btn-danger">{{$message}}</button>
+                            <button class="btn-danger">{{ $message }}</button>
                             @enderror
                         </div>
                         <div class="form-group">
                             <label>Product name :</label>
-                            <input type="text" name="product_name" class="form-control" class="@error('product_name') is-invalid @enderror"
-                                   placeholder="" value="{{$product->product_name}}">
+                            <input type="text" name="product_name" class="form-control"
+                                class="@error('product_name') is-invalid @enderror" placeholder=""
+                                value="{{ $product->product_name }}">
                             @error('product_name')
-                            <button class="btn-danger">{{$message}}</button>
+                            <button class="btn-danger">{{ $message }}</button>
                             @enderror
                         </div>
                         <div class="form-group">
                             <label>Unit price :</label>
-                            <input type="text" name="unit_price" class="form-control" class="@error('unit_price') is-invalid @enderror"
-                                   placeholder="" value="{{$product->unit_price}}">
+                            <input type="text" name="unit_price" class="form-control"
+                                class="@error('unit_price') is-invalid @enderror" placeholder=""
+                                value="{{ $product->unit_price }}">
                             @error('unit_price')
-                            <button class="btn-danger">{{$message}}</button>
+                            <button class="btn-danger">{{ $message }}</button>
                             @enderror
                         </div>
                         <button type="submit" id="submit" class="btn btn-primary">Save</button>
