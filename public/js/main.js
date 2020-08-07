3cd0a975f54a853767ead43adda3b6b57b45aba5
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-08-07 13:24:12 +0200

    Added a function for getting unit price using jquery and ajax

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,29 @@
 $(function () {
-    $('#cat_id').on('change', function () {
-        var cat_id = $(this).val();
-        $.get(route + '/orders/getProducts', {cat_id:cat_id}, function(data){
-                $('#product_id').html(data);
-        });
+   $('#cat_id').on('change', function () {
+      var cat_id = $(this).val();
+      $.get(route + '/orders/getProducts', { cat_id: cat_id }, function (data) {
+         $('#product_id').html(data);
+      });
 
-        // if (cat_id) {
-        //     $.ajax({
-        //         type: 'GET',
-        //         async: true,
-        //         url: route + '/orders/getProducts',
-        //         data: 'cat_id=' + cat_id,
-        //         success: function (data) {
-        //             $('#product_id').html(data);
-        //         }
-        //     });
-        // }
-    });
+      // if (cat_id) {
+      //     $.ajax({
+      //         type: 'GET',
+      //         async: true,
+      //         url: route + '/orders/getProducts',
+      //         data: 'cat_id=' + cat_id,
+      //         success: function (data) {
+      //             $('#product_id').html(data);
+      //         }
+      //     });
+      // }
+   });
+});
+
+$(function () {
+   $('#product_id').on('change', function () {
+      var product_id = $(this).val();
+      $.get(route + '/orders/getUnitPrice', { product_id: product_id }, function (data) {
+         $('#unit_price').html(data);
+      });
+   });
 });
