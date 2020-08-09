c8ec292381cb435b5fb1ba3c4e97678168240753
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-08-09 11:21:31 +0200

    Removed input field. Replaced by its value

--- resources/views/orders/getUnitPrice.blade.php
+++ resources/views/orders/getUnitPrice.blade.php
@@ -1,7 +1,1 @@
-<label for="unit_price">Unit price :</label>
-<input type="text" name="unit_price" id="price" class="form-control"
-       class="@error('unit_price') is-invalid @enderror"
-       placeholder="" value="{{$product->unit_price}}">
-@error('unit_price')
-<button class="btn-danger">{{$message}}</button>
-@enderror
+{{$product->unit_price}}
