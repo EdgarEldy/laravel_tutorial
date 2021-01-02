9de2d182f37180c2fd9fe6a77d5081b9cb71b847
Edgar Eldy <ed.eldy21@live.com>
2021-01-02 20:56:51 +0200

    Change panel heading to AUTHENTIFICATION

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
 <div class="row">
     <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
         <div class="login-panel panel panel-default">
-            <div class="panel-heading">Laravel tutorial | STORE MANAGEMENT SYSTEM  </div>
+            <div class="panel-heading">Laravel tutorial | AUTHENTIFICATION  </div>
             <div class="panel-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
