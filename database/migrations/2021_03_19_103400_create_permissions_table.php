b38f7458d1cf6ea93433fbfbf04e70d4f62d2a45
Edgar Eldy <ed.eldy21@live.com>
2021-03-23 23:33:17 +0200

    Update permissions table. Add permission name column

--- database/migrations/2021_03_19_103400_create_permissions_table.php
+++ database/migrations/2021_03_19_103400_create_permissions_table.php
@@ -15,6 +15,7 @@ class CreatePermissionsTable extends Migration
     {
         Schema::create('permissions', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name', 100)->unique();
             $table->timestamps();
         });
     }
