4597d5bccaf82a79f5359a3e5d54d2916c9ef387
Edgar Eldy <ed.eldy21@live.com>
2021-03-23 23:39:52 +0200

    Add belongsToMany users relationship

--- app/Role.php
+++ app/Role.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Role extends Model
 {
-    //
+    //Add belongsToMany users relationship
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
