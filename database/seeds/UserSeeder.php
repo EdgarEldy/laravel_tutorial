0a836f65736d9a892429a5d32d65ac4448b3c767
Edgar Eldy <ed.eldy21@live.com>
2021-03-31 22:30:42 +0200

    Populate the pivot table 'role_user'

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -17,10 +17,17 @@ class UserSeeder extends Seeder
         //Populate fake users
         User::factory()->count(10)->create();
 
-         //Get all the roles syncing up to 3 random roles to each user except user_id=1('admin')
-         $roles = Role::all();
+        //Get all the roles syncing up to 3 random roles to each user except user_id=1('admin')
+        $roles = Role::all();
 
-         //Get all the permissions syncing up to random permissions to each role except role_id=1('admin')
+        //Get all the permissions syncing up to random permissions to each role except role_id=1('admin')
         $permissions = Permission::all();
+
+        //Populate the pivot table 'role_user'
+        User::where('id', '!=', 1)->get()->each(function ($user) use ($roles) {
+            $user->roles()->sync(
+                $roles->random(rand(1, 3))->pluck('id')->toArray()
+            );
+        });
     }
 }
