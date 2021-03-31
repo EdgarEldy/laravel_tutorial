f1c941b09b72b524d57187747b5202ba2445beb6
Edgar Eldy <ed.eldy21@live.com>
2021-03-31 22:11:53 +0200

    Add role_id foreign key

--- database/migrations/2021_03_23_213044_create_permission_role_table.php
+++ database/migrations/2021_03_23_213044_create_permission_role_table.php
@@ -18,6 +18,11 @@ class CreatePermissionRoleTable extends Migration
             $table->unsignedBigInteger('role_id');
             $table->unsignedBigInteger('permission_id');
             $table->timestamps();
+
+            $table->foreign('role_id')
+                ->references('id')
+                ->on('roles')
+                ->onDelete('cascade');
         });
     }
 
