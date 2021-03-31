37668764e9cc8f49cf3300ac10681156abf7421c
Edgar Eldy <ed.eldy21@live.com>
2021-03-31 22:28:04 +0200

    Populate fake roles

--- database/seeds/RoleSeeder.php
+++ database/seeds/RoleSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Role;
 use Illuminate\Database\Seeder;
 
 class RoleSeeder extends Seeder
@@ -11,6 +12,7 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Populate fake roles
+        Role::factory()->count(5)->create();
     }
 }
