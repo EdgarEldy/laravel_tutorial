f735f0eb1b1507148a7399b2773d86fe27f2e8b8
Edgar Eldy <ed.eldy21@live.com>
2021-03-31 22:06:24 +0200

    Fixed foreign key bug

--- database/migrations/2021_03_19_103215_create_role_user_table.php
+++ database/migrations/2021_03_19_103215_create_role_user_table.php
@@ -15,8 +15,8 @@ class CreateRoleUserTable extends Migration
     {
         Schema::create('role_user', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->foreignId('user_id')->constrained('users');
-            $table->foreignId('role_id')->constrained('roles');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('role_id');
             $table->timestamps();
         });
     }
--- database/migrations/2021_03_23_213044_create_permission_role_table.php
+++ database/migrations/2021_03_23_213044_create_permission_role_table.php
@@ -15,8 +15,8 @@ class CreatePermissionRoleTable extends Migration
     {
         Schema::create('permission_role', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->foreignId('role_id')->constrained('roles');
-            $table->foreignId('permission_id')->constrained('permissions');
+            $table->unsignedBigInteger('role_id');
+            $table->unsignedBigInteger('permission_id');
             $table->timestamps();
         });
     }
