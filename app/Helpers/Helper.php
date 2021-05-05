7d2df6c030cfcc32b5cc568a33b32ba349ea6da5
Edgar Eldy <ed.eldy21@live.com>
2021-05-05 12:19:53 +0200

    Setting up date format

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -1,5 +1,5 @@
 <?php
-
+use Carbon\Carbon;
 // Setting up flash messages
 if (!function_exists('flash')) {
     function flash(String $message, String $type = 'success', String $icon = 'check')
@@ -7,5 +7,13 @@ if (!function_exists('flash')) {
         session()->flash('status.message', $message);
         session()->flash('status.type', $type);
         session()->flash('status.icon', $icon);
+    }
+}
+
+// Setting up date format
+if (!function_exists('format_date_en')) {
+    function format_date_en($date)
+    {
+        return Carbon::parse($date)->format('d/m/Y at H:i:s');
     }
 }
