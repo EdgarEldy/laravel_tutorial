466a58e85653a5221c3625b2b8710c773970369b
Edgar Eldy <ed.eldy21@live.com>
2021-05-05 12:18:08 +0200

    Setting up flash messages

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -1,1 +1,11 @@
 <?php
+
+// Setting up flash messages
+if (!function_exists('flash')) {
+    function flash(String $message, String $type = 'success', String $icon = 'check')
+    {
+        session()->flash('status.message', $message);
+        session()->flash('status.type', $type);
+        session()->flash('status.icon', $icon);
+    }
+}
