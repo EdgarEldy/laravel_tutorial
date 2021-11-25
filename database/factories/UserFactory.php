0d68d12194b1db4ba1a292cee5cd60d0f2fcfb6a
Edgar Eldy <ed.eldy21@live.com>
2021-11-25 21:38:06 +0200

    Changed password encryption

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@
 
 use App\User;
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /*
@@ -24,7 +25,7 @@ $factory->define(User::class, function (Faker $faker) {
         'email' => $this->faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'username' => $this->faker->unique()->userName,
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => Hash::make('12345678'),
         'remember_token' => Str::random(10),
     ];
 });
