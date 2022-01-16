ca542adeb379ba2db40db61e95b09a6bfc54c9de
Edgar Eldy <ed.eldy21@live.com>
2022-01-16 21:19:42 +0200

    Add chosen.min.css library and format document

--- resources/views/inc/head.blade.php
+++ resources/views/inc/head.blade.php
@@ -1,20 +1,21 @@
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>LARAVEL TUTORIAL</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>LARAVEL TUTORIAL</title>
 
-<link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
-<link href="{{asset('css/datepicker3.css')}}" rel="stylesheet">
-<link href="{{asset('css/bootstrap-table.css')}}" rel="stylesheet">
-<link href="{{asset('css/styles.css')}}" rel="stylesheet">
-<link href="{{asset('css/fontawesome.min.css')}}" rel="stylesheet" type="text/css" >
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('css/datepicker3.css')}}" rel="stylesheet">
+    <link href="{{asset('css/bootstrap-table.css')}}" rel="stylesheet">
+    <link href="{{asset('css/styles.css')}}" rel="stylesheet">
+    <link href="{{asset('css/fontawesome.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('css/chosen.min.css')}}" rel="stylesheet">
 
-<!--Icons-->
-<script src="{{asset('js/lumino.glyphs.js')}}"></script>
+    <!--Icons-->
+    <script src="{{asset('js/lumino.glyphs.js')}}"></script>
 
-<!--[if lt IE 9]>
-<script src="js/html5shiv.js"></script>
-<script src="js/respond.min.js"></script>
-<![endif]-->
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
 
 </head>
